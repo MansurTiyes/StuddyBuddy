25ef98ca10f54d00b7f4c545d0a4a323260362d5
Mansur Tiyes <tiyes@usc.edu>
2025-07-21 00:32:00 +0200

    Initial Commit

---
+++ src/test/java/com/example/studdybuddy/StuddyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studdybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuddyBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
